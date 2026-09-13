45a35da12c7c5c5fab4116a5d4805f7d13e9590b
Vinicius Miranda de Araujo <vinicius.araujo.1457564@sga.pucminas.br>
2026-09-12 22:30:54 -0300

    feat: criação da estrutura inicial do projeto

---
+++ src/main/java/com/puc/cg/App.java
@@ -0,0 +1,14 @@
+package com.puc.cg;
+
+import com.puc.cg.ui.screens.MainFrame;
+
+import javax.swing.SwingUtilities;
+
+public class App {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            MainFrame frame = new MainFrame();
+            frame.setVisible(true);
+        });
+    }
+}
